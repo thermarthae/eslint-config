c5d9c631d0a32c838e70f3159a980aed7cf02c45
thermarthae <thermarthae@protonmail.com>
2024-10-22 23:36:26 +0200

    Add basic TS types

---
+++ base.d.ts
@@ -0,0 +1,5 @@
+import { Linter } from 'eslint';
+
+const base: Linter.Config[];
+
+export default base;
---
+++ index.d.ts
@@ -0,0 +1,5 @@
+import { Linter } from 'eslint';
+
+const main: Linter.Config[];
+
+export default main;
